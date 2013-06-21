b2e87c07f77adfa8b879440d1f7234258e3b2044
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-21 12:22:31 -0500

    cleaned up files

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,6 @@ require 'rubygems'
 
 require 'uri'
 require 'pathname'
-# require 'bcrypt'
 
 require 'pg'
 require 'active_record'
@@ -20,8 +19,6 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
-
-# require 'faker'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
